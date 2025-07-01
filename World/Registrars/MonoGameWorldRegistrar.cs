88a1510de5768dd82ccb348b7a6dfae33e8d502b
XerShade <xershade.ca@gmail.com>
2025-07-01 19:33:00 -0400

    refactor: implement autofac

--- World/Registrars/MonoGameWorldRegistrar.cs
+++ World/Registrars/MonoGameWorldRegistrar.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Reoria.Engine.Container.Registrars;
-using Reoria.Engine.Container.Services.Interfaces;
 using Reoria.Engine.MonoGame.World.Interfaces;
 
 namespace Reoria.Engine.MonoGame.World.Registrars;
@@ -13,9 +14,7 @@ public class MonoGameWorldRegistrar : IServiceRegistrar
     /// <summary>
     /// Registers MonoGame world-related services into the dependency injection container.
     /// </summary>
-    /// <param name="registryGuard">
-    /// A service registry guard used to prevent duplicate registrations and ensure safe registration.
-    /// </param>
-    public void RegisterServices(IServiceRegistryGuard registryGuard)
-        => registryGuard.TryRegister<IMonoGameWorldBuilder, MonoGameWorldBuilder>(ServiceLifetime.Transient);
+    /// <inheritdoc/>
+    public void RegisterServices(ContainerBuilder builder, IConfiguration configuration, ILoggerFactory loggerFactory)
+        => builder.RegisterType<MonoGameWorldBuilder>().As<IMonoGameWorldBuilder>().InstancePerDependency();
 }
